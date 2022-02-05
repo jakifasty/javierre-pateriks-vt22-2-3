2d1017f848465a2d11a5b90ca2960542fb202965
Patrik <pateriks@kth.se>
2022-02-05 02:22:12 +0100

    tw1 import

--- src/DinnerModel.js
+++ src/DinnerModel.js
@@ -7,14 +7,17 @@ class DinnerModel{
         this.dishes= dishArray;
     }
     setNumberOfGuests(nr){
-        // if() and throw exercise
-        
+        if(Number.isInteger(nr) & nr>0){
+          this.numberOfGuests = nr;
+        }else{
+          throw new Error ('number of guests not a positive integer');
+        }
         // TODO throw an error if the argument is smaller than 1 or not an integer
         // the error message must be exactly "number of guests not a positive integer"
         // to check for integer: test at the console Number.isInteger(3.14)
 
         // TODO if the argument is a valid number of guests, store it in this.numberOfGuests
-        
+
         // when this is done the TW1.1 DinnerModel "can set the number of guests" should pass
         // also "number of guests is a positive integer"
     }
@@ -23,26 +26,38 @@ class DinnerModel{
         // It sets this.dishes to a new array [   ] where we spread (...) the previous value
         this.dishes= [...this.dishes, dishToAdd];
     }
-    
+
     removeFromMenu(dishToRemove){
         // callback exercise! Also return keyword exercise
         function hasSameIdCB(dish){
+          return dishToRemove.id !== dish.id;
             // TODO return true if the id property of dish is _different_ from the dishToRemove's id property
             // This will keep the dish when we filter below.
             // That is, we will not keep the dish that has the same id as dishToRemove (if any)
         }
-        this.dishes= this.dishes.filter(/*TODO pass the callback!*/);
+        this.dishes= this.dishes.filter(hasSameIdCB/*TODO pass the callback!*/);
         // the test "can remove dishes" should pass
     }
-    /* 
+    /*
        ID of dish currently checked by the user.
-       A strict MVC/MVP Model would not keep such data, 
-       but we take a more relaxed, "Application state" approach. 
+       A strict MVC/MVP Model would not keep such data,
+       but we take a more relaxed, "Application state" approach.
        So we store also abstract data that will influence the application status.
      */
-    setCurrentDish(id){
-        //this.currentDish=TODO
-    }
+     setCurrentDish(id){
+         this.currentDish= id;
+     }
+     removeDish(id){
+       // callback exercise! Also return keyword exercise
+       function hasSameIdCB(dish){
+         return id !== dish.id;
+           // TODO return true if the id property of dish is _different_ from the dishToRemove's id property
+           // This will keep the dish when we filter below.
+           // That is, we will not keep the dish that has the same id as dishToRemove (if any)
+       }
+       this.dishes= this.dishes.filter(hasSameIdCB/*TODO pass the callback!*/);
+       // the test "can remove dishes" should pass
+     }
 
 }
 
