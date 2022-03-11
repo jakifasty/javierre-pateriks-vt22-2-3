78a32922e05e3dd85ca8c558b50b9ee55d93d489
cristi <cristi@kth.se>
2022-03-11 23:33:47 +0100

    reduced the indexes because many imports are done by the root component

--- src/reactjs/index.js
+++ src/reactjs/index.js
@@ -11,14 +11,9 @@ import "firebase/database";
 // needed for plugging in a "mock" firebase for testing. In the project simply import firebase where needed, as above
 window.firebase=firebase;
 
-// uncomment when you implemented firebaseModel in TW3.5. require() is needed, to use window.firebase above
-//const {firebaseModelPromise, updateFirebaseFromModel, updateModelFromFirebase}=require("/src/firebaseModel.js");
+// TODO import navigation
 
-// require() because the lab App loads React/Vue presenters
-const App=require("/src/views/app.js").default;
+// using require() instead of import, for the above assignments to take effect before ReactRoot is loaded
+const Reactoot=require("/src/reactjs/ReactRoot.js").default;
 
-// import DinnerModel, navigation,
-
-// import promiseNoData, you will need it during resolve of firebaseModelPromise
-
-// render a ReactRoot that resolves firebaseModelPromise, then displays the App (see tw/tw3.5-react.js)
+// TODO render ReactRoot into the "root" div
--- src/vuejs/index.js
+++ src/vuejs/index.js
@@ -10,15 +10,9 @@ import "firebase/database";
 // needed for plugging in a "mock" firebase for testing. In the project simply import firebase where needed, as above
 window.firebase=firebase;
 
-// uncomment when you implemented firebaseModel in TW3.5. require() is needed, to use window.firebase above
-//const {firebaseModelPromise, updateFirebaseFromModel, updateModelFromFirebase}=require("/src/firebaseModel.js");
+// TODO import navigation
 
-// require() because the lab App loads React/Vue presenters
-const App=require("/src/views/app.js").default;
+// using require() instead of import, for the above assignments to take effect before VueRoot is loaded
+const VueRoot=require("/src/vuejs/VueRoot.js").default;
 
-// import DinnerModel, navigation,
-
-// import promiseNoData, you will need it during resolve of firebaseModelPromise
-// resolvePromise may be useful as well!
-
-// render a VueRoot that resolves firebaseModelPromise, then displays the App (see tw/tw3.5.js)
+// TODO render VueRoot into the "root" div
